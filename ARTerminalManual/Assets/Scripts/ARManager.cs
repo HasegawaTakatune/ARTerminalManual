7d76ecbea54ce140b2355af74bc25f78b5fb48f8
Takatune <0ta5274738t4p4r@gmail.com>
2019-12-18 23:45:12 +0900

    接続端子の詳細説明モード時の表示作成

---
+++ ARTerminalManual/Assets/Scripts/ARManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GoogleARCore;
+using UnityEngine;
+
+public class ARManager : MonoBehaviour
+{
+    private List<AugmentedImage> augmentedImages = new List<AugmentedImage>();
+
+    [SerializeField] private GameObject prefab = default;
+
+    private GameObject ARObj = default;
+
+    private void Update()
+    {
+        // トラッキングできなかった場合何もしない
+        if(Session.Status != SessionStatus.Tracking)
+            return;
+
+        // ARCoreがトラッキングしているものを取得する。
+        Session.GetTrackables<AugmentedImage>(augmentedImages, TrackableQueryFilter.Updated);
+
+        foreach(AugmentedImage image in augmentedImages)
+        {
+            if(image.TrackingState == TrackingState.Tracking)
+            {
+                if(ARObj == null)
+                {
+                    // トラッキングを始めたらオブジェクトを生成する
+                    Anchor anchor = image.CreateAnchor(image.CenterPose);
+                    ARObj = Instantiate(prefab, anchor.transform);
+                }
+                else
+                {
+                    // トラッキング中は位置・回転を調節する
+                    ARObj.transform.position = image.CenterPose.position;
+                    ARObj.transform.rotation = image.CenterPose.rotation;
+                }
+            }
+        }
+    }
+}
---
+++ ARTerminalManual/Assets/Scripts/Manual.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Manual : MonoBehaviour
+{
+
+    [SerializeField] private string Name;
+
+    [SerializeField, TextArea] private string Explanation;
+
+    [SerializeField] private Image[] RealImage = new Image[2];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
